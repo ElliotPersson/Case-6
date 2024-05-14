76a3abdf29d38540536b20430c31524cb270795f
Elliot Persson <elliot.thomas.persson@gmail.com>
2024-05-14 08:16:04 +0200

    Redirect to home.php upon correct database credentials

--- app/public/login.php
+++ app/public/login.php
@@ -13,14 +13,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
+    
 
     try {
 
-       
+       $stmt = $conn->prepare("SELECT * FROM user WHERE user_name=:username AND user_password=:password");
+
+       $stmt->bindParam(':username', $username);
+       $stmt->bindParam(':password', $password);
+
+       $stmt->execute();
+
+       if ($stmt->rowCount() == 1) {
+
+        $_SESSION['username'] = $username;
+
+        header("Location: home.php");
+        exit();
+
+       } else {
+
+        echo "Invalid username or password!";
+       }
 
     } catch(PDOException $e) {
 
-        echo "Connection failed: " . $e->getMessage();
+        echo "Error:  " . $e->getMessage();
     }
 
 }
