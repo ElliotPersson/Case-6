7cd13587ceddd68317d708c8ce988c0b42d0ea9a
Elliot Persson <elliot.thomas.persson@gmail.com>
2024-05-25 16:47:08 +0200

    made it so only the user who made the business can edit or delete it

--- app/public/view_business.php
+++ app/public/view_business.php
@@ -23,8 +23,10 @@ if(!isset($_SESSION['user_id'])) {
     <?php
     require("db_connection.php");
 
-    $sql = "SELECT business.*, category.category_name FROM business
-    JOIN category ON business.category_id = category.id";
+    $sql = "SELECT business.*, category.category_name, user.user_name AS owner_name 
+    FROM business
+    JOIN category ON business.category_id = category.id
+    JOIN user ON business.user_id = user.id";
 
     $stmt = $conn->prepare($sql);
     $stmt->execute();
@@ -38,17 +40,24 @@ if(!isset($_SESSION['user_id'])) {
         echo "<strong>Address:</strong> {$business['address']}";
         echo "<strong>Open Hours:</strong> {$business['open_hours']}";
         echo "<strong>Category:</strong> {$business['category_name']}";
+        echo "<strong>Owner:</strong> {$business['owner_name']}";
 
-        echo "<form action='delete_business.php' method='post' onsubmit='return confirmDelete()'>";
-        echo "<input type='hidden' name='business_id' value='{$business['id']}'> ";
-        echo "<button type='submit'>Delete</button>";
-        echo "</form>";
+        if ($_SESSION['user_id'] == $business['user_id']) {
+            echo "<form action='delete_business.php' method='post' onsubmit='return confirmDelete()'>";
+            echo "<input type='hidden' name='business_id' value='{$business['id']}'> ";
+            echo "<button type='submit'>Delete</button>";
+            echo "</form>";
+    
+            
+            echo "<a href='edit_business.php?id={$business['id']}'>Edit</a>";   
+        }
 
-        
-        echo "<a href='edit_business.php?id={$business['id']}'>Edit</a>";   
+       
 
         echo "</div>";
+
     }
     ?>
+    
 </body>
 </html>
