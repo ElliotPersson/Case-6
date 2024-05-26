5f75012c1a8ed38fc1b1b41dd7c6854ed61cc062
Elliot Persson <elliot.thomas.persson@gmail.com>
2024-05-26 20:45:54 +0200

    made some changes, added screenshot folder, created setup.php that create user table if it doesnt exist

--- app/public/login.php
+++ app/public/login.php
@@ -1,31 +1,53 @@
 <?php
+
+/* Startar en session: */
 session_start();
+
+/* Inkluderar db_connection.php som innehåller logiken för databas uppkoppling: */
 require("db_connection.php");
 
+/* Kollar om det är en post metod: */
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    /* Tar användarnamnet och lösenordet från formuläret och lägger det i variabler: */
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     try {
+        /* Väljer alla kolumner från user tabellen där user_name kolumnen matchar med användarnamnet: */
         $sql = "SELECT * FROM user WHERE user_name = :username";
+        
+        /* förbereder sql innan den körs för säkerhet: */
         $stmt = $conn->prepare($sql);
+
         $stmt->execute([':username' => $username]);
 
+        /* Kollar om användaren finns i databasen: */
         if ($stmt->rowCount() == 1) {
+            
+            /* Fetchar all information om användaren och lägger den i $user: */
             $user = $stmt->fetch();
 
+            /* Kollar om lösenordet matchar: */
             $password_match = password_verify($password, $user['user_password']);
+            
             if ($password_match) {
+
+                /* Lägger användarens id och användarnamn i variabler: */
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['username'] = $user['user_name'];
+
+                /* Skickas till home.php */
                 header("Location: home.php");
                 exit();
+            
+                /* Om det inte matchar: */
             } else {
-                echo "Password doesn't match!";
+                echo "Wrong password!";
                 exit;
             }
         } else {
-            echo "<div class='error-message'>Invalid username or password!</div>";
+            echo "<div class='error-message'>That user doesnt exist!</div>";
         }
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
---
+++ app/public/setup.php
@@ -0,0 +1,11 @@
+<?
+
+require "db_connection.php";
+
+$sql= "CREATE TABLE IF NOT EXISTS `user` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_name` VARCHAR(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    `user_password` VARCHAR(255) COLLATE utf8mb4_general_ci DEFAULT NULL
+  );";
+
+$pdo->exec($sql);
